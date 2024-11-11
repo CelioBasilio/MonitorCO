7bba829588ac6bd305180dfed6d4b6e8eead641d
Lucas Alves Azevedo <lucasaalvesazevedo@gmail.com>
2024-11-10 22:33:54 -0300

    Revert "Merge branch 'master' into brokerThingspeak"
    This reverts commit 1012e79d80cc60950b2653b3fe4d3429db4115a7, reversing
    changes made to 92109b73d058a631bf8aa9515f2a65868f2e9183.

---
+++ app/src/main/java/com/example/monitorco/model/Usuario.kt
@@ -0,0 +1,9 @@
+package com.example.monitorco.model
+
+data class Usuario(
+    val nome: String,
+    val endereco: String,
+    val email: String,
+    val senha: String,
+    val cnpj: String
+)
--- app/src/main/java/com/example/monitorco/utils/Utils.kt
+++ app/src/main/java/com/example/monitorco/utils/Utils.kt
@@ -4,12 +4,11 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import com.google.firebase.FirebaseApp
-import com.google.firebase.firestore.FirebaseFirestore
+import com.example.monitorco.api.RetrofitInstance
+import com.example.monitorco.model.Login
 
 object Utils {
 
-    // Verifica se há conexão de rede disponível
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -22,17 +21,8 @@ object Utils {
         }
     }
 
-    // Inicializa o Firebase (para garantir que ele esteja pronto antes do uso)
-    fun initializeFirebase(context: Context) {
-        try {
-            FirebaseApp.initializeApp(context)
-            FirebaseFirestore.getInstance() // Para garantir que o Firestore esteja pronto
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
 
-    // Validação de CNPJ
+
     fun isCNPJValido(cnpj: String): Boolean {
         val cnpjLimpo = cnpj.replace("[^0-9]".toRegex(), "")
         if (cnpjLimpo.length != 14) return false
@@ -50,4 +40,17 @@ object Utils {
         val resto = soma % 11
         return if (resto < 2) 0 else 11 - resto
     }
+
+    suspend fun isServerAvailable(email: String, senha: String): Boolean {
+        val login = Login(email, senha)
+        return try {
+            val response = RetrofitInstance.api.login(login)
+            response.isSuccessful
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+
+    // Outras funções de validação podem ser adicionadas aqui
 }
