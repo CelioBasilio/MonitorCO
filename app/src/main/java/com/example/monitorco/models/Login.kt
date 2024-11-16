326329f315f68a923a830a2988ad627993dd43a3
Lucas Alves Azevedo <38042844+LucasMeninola@users.noreply.github.com>
2024-11-16 11:27:07 -0300

    Delete app directory

--- app/src/main/java/com/example/monitorco/models/Login.kt
+++
@@ -1,7 +0,0 @@
-package com.example.monitorco.models
-
-// Classe de modelo que representa um login de usuário.
-data class Login(
-    val email: String, // Endereço de email do usuário.
-    val senha: String  // Senha do usuário.
-)
